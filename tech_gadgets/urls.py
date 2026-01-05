aa9ed50993c5f5c147f9e9b7955aa03a0fbfe955
alexschoefer <alex.schoefer@gmx.net>
2026-01-05 21:02:02 +0100

    feat: show jsonResponse

--- tech_gadgets/urls.py
+++ tech_gadgets/urls.py
@@ -3,5 +3,5 @@ from .views import start_page_view, single_gadget_view
 
 urlpatterns = [
     path('', start_page_view),
-    path('gadget/', single_gadget_view)
+    path('gadget/<int: gadget_id>', single_gadget_view)
 ]
--- tech_gadgets/views.py
+++ tech_gadgets/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+import json
 
 from .dummy_data import gadgets
 
@@ -9,5 +10,5 @@ def start_page_view(request):
     return HttpResponse("Hey das hat doch gut funktioniert!")
 
 
-def single_gadget_view(request):
-    return HttpResponse(gadgets[0])
+def single_gadget_view(request, gadget_id):
+    return JsonResponse((gadgets[gadget_id]))
