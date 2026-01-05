77ad6133bb802039351bb92c7d7f4d2f604f4302
alexschoefer <alex.schoefer@gmx.net>
2026-01-05 20:39:28 +0100

    feat: add dummy data

--- tech_gadgets/urls.py
+++ tech_gadgets/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import start_page_view
+from .views import start_page_view, single_gadget_view
 
 urlpatterns = [
-    path('', start_page_view)
+    path('', start_page_view),
+    path('gadget/', single_gadget_view)
 ]
--- tech_gadgets/views.py
+++ tech_gadgets/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .dummy_data import gadgets
+
 # Create your views here.
 
 def start_page_view(request):
     return HttpResponse("Hey das hat doch gut funktioniert!")
+
+
+def single_gadget_view(request):
+    return HttpResponse(gadgets[0])
