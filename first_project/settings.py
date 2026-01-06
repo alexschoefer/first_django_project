ef930f868f8d65aa0e202eb16d38bcc6ec23bb07
alexschoefer <alex.schoefer@gmx.net>
2026-01-06 13:10:40 +0100

    feat: add some functions in views.py for practice; including an 404.html

--- first_project/settings.py
+++ first_project/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = 'first_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'first_project/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- tech_gadgets/views.py
+++ tech_gadgets/views.py
@@ -1,14 +1,67 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.shortcuts import redirect
+from django.http import HttpResponse, JsonResponse, HttpResponseNotFound, Http404
 import json
+from django.utils.text import slugify
+from django.urls import reverse
+
+from django.views import View
 
 from .dummy_data import gadgets
+
+from django.views.generic.base import RedirectView
 
 # Create your views here.
 
 def start_page_view(request):
     return HttpResponse("Hey das hat doch gut funktioniert!")
 
+class RedirectToGadgetView(RedirectView):
+    pattern_name = "gadget_slug_url"
 
-def single_gadget_view(request, gadget_id):
-    return JsonResponse((gadgets[gadget_id]))
+    def get_redirect_url(self, *args, **kwargs):
+        slug = slugify(gadgets[kwargs.get("gadget_id", 0)]["name"])
+        new_kwargs = {"gadget_slug": "ultraportable_laptop-z"}
+        return super().get_redirect_url(*args, **new_kwargs)
+
+def single_gadget_int_view(request, gadget_id):
+    if len(gadgets) > gadget_id:
+        new_slug = slugify(gadgets[gadget_id]["name"])
+        new_url = reverse("gadget_slug_url", args=[new_slug])
+        #return JsonResponse({"result": slugify(gadgets[1]['name'])})
+        return redirect(new_url)
+    return HttpResponseNotFound("not found by me")
+
+
+class GatgetView(View):
+    def get(self, request, gadget_slug):
+        gadget_match = None 
+        for gadget in gadgets:
+            if slugify(gadget["name"]) == gadget_slug:
+                gadget_match = gadget
+
+        if gadget_match:
+            return JsonResponse(gadget_match)
+        raise Http404
+
+    def post(self, request, *args, **kwargs):
+        try:
+            data = json.loads(request.body)
+            print(f"recieved data: {data["test"]}")
+            return JsonResponse({"response": "Das war was"})
+        except:
+            return JsonResponse({"response": "Das war wohl nichts"})
+
+
+def single_gadget_view(request, gadget_slug=""):
+    if request.method == "GET":
+        gadget_match = None 
+        for gadget in gadgets:
+            if slugify(gadget["name"]) == gadget_slug:
+                gadget_match = gadget
+
+        if gadget_match:
+            return JsonResponse(gadget_match)
+        raise Http404
+
+    if request.method == "POST":
+        pass
